bd321b6699ddb50e0d484002e6781303cef33c50
Juvenal Brenes <juvenal.brenes@gmail.com>
2026-07-04 11:53:09 -0400

    Simplificar configuración de construcción del sitio

--- setup.ps1
+++
@@ -1,7 +0,0 @@
-python -m venv .venv
-.\.venv\Scripts\Activate.ps1
-pip install --upgrade pip
-pip install mkdocs mkdocs-material
-
-Write-Host "Entorno local de LEY.DO listo."
-Write-Host "Ejecutar: .\.venv\Scripts\Activate.ps1 ; mkdocs serve"
